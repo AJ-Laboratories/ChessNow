b27f13c3e8907a662c7576bc43653cd12a0ebb85
AlexPanayotov <alexanderpanayotov98@gmail.com>
2014-11-23 00:15:07 +0100

    code

--- ChessNow/ChessNow/ViewController.swift
+++ ChessNow/ChessNow/ViewController.swift
@@ -11,9 +11,72 @@ import SpriteKit
 
 class ViewController: UIViewController {
 
+    var cellWidth = 57
+    var cellHeight = 57
+    var squares = 64
+    var leftX = 100
+    var topY = 100
+    
+    var row = 0,col=0;
+    var firstColor = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        cellWidth = 44;
+        cellHeight = cellWidth;
+        topY = 330-4*cellHeight;
+        leftX = 187-4*cellWidth;
+        
+/* for (i=1; i<=64; i++) {
+if (firstColor == 0) {
+gridCell[i] = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"graySquare.png"]];
+} else {
+gridCell[i] = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"whiteSquare.png"]];
+}
+
+onTopOfCell[49] = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"whitePawn.png"]];
+
+gridCell[i].frame = CGRectMake(leftX+cellWidth*col, topY+cellHeight*row, cellWidth, cellHeight);
+[self.view addSubview:gridCell[i]];
+onTopOfCell[i].frame = CGRectMake(leftX+cellWidth*col, topY+cellHeight*row, cellWidth, cellHeight);
+[self.view addSubview:onTopOfCell[i]];
+
+col= col+1;
+firstColor = 1- firstColor;
+if(col >7) {
+row += 1;
+firstColor = 1-firstColor;
+col = 0;
+}
+
+gridCell[i].userInteractionEnabled = YES;
+gridCell[i].multipleTouchEnabled = YES;
+
+}
+*/
+        
     }
+    
+    /*
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+UITouch *touch = [[event allTouches] anyObject];
+
+int i;
+for (i=1; i<=squares; i++) {
+if ([touch view] ==gridCell[i]) {
+onTopOfCell[49].frame = gridCell[41].frame;
+[self.view addSubview:onTopOfCell[i]];
+[self.view addSubview:gridCell[i]];
+
+
+}
+}
+
+}
+
+*/
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()

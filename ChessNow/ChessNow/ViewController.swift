4c0a58363ee5362e580416408f0a31636d6843b6
b3rge <johannesb98@gmail.com>
2014-12-10 19:47:00 +0100

    s
    s

--- ChessNow/ChessNow/ViewController.swift
+++ ChessNow/ChessNow/ViewController.swift
@@ -9,23 +9,31 @@
 import UIKit
 import SpriteKit
 
+let screenSize: CGRect = UIScreen.mainScreen().bounds
+let screenWidth = screenSize.width
+let screenHeight = screenSize.height
+let pieceSize = sqrt(screenWidth * screenWidth / 64)
+
 
 
 class ViewController: UIViewController {
 	
 	let objc = Objective_C()
 
+    
     @IBOutlet weak var image1: UIImageView!
-    @IBAction func button(sender: AnyObject) {
+	
+	@IBOutlet weak var chessBoard: UIImageView!
+
+    @IBAction func button(sender: AnyObject)
+	{
         
         var positionx = image1.frame.origin.x
         var positiony = image1.frame.origin.y
-        
-        image1.frame = CGRect(x: positionx, y: positiony, width: image1.frame.size.width, height: image1.frame.size.height)
-        
-        positiony += -20
-
-                image1.frame = CGRect(x: positionx, y: positiony, width: image1.frame.size.width, height: image1.frame.size.height)
+		
+        image1.frame = CGRect(x: positionx, y: positiony, width: pieceSize, height: pieceSize)
+        positiony -= screenWidth  / 8
+		image1.frame = CGRect(x: positionx, y: positiony, width: pieceSize, height: pieceSize)
     }
     
 	override func viewDidLoad() {
@@ -35,10 +43,12 @@ class ViewController: UIViewController {
 		var nav = self.navigationController?.navigationBar
 		nav?.barStyle = UIBarStyle.BlackTranslucent
         
-		let screenSize: CGRect = UIScreen.mainScreen().bounds
-        let screenWidth = screenSize.width
-        let screenHeight = screenSize.height
-        
+//		var startPositionx = chessBoard.frame.origin.x
+//		var startPositiony = chessBoard.frame.origin.y
+//		image1.frame = CGRect(x: startPositionx, y: startPositiony, width: pieceSize, height: pieceSize)
+
+		
+		
         println("\(screenHeight) is the height and \(screenWidth) is the width. \(screenSize) is the screensize")
         
     }

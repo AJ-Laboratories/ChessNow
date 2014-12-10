e5fa8360112b0fac24580b6d08cfe92ae5ea6ac2
b3rge <johannesb98@gmail.com>
2014-12-10 19:15:00 +0100

    ss
    ss

--- ChessNow/ChessNow/ViewController.swift
+++ ChessNow/ChessNow/ViewController.swift
@@ -9,15 +9,40 @@
 import UIKit
 import SpriteKit
 
+
+
 class ViewController: UIViewController {
 	
 	let objc = Objective_C()
-	
-	override func viewDidLoad() {
-        objc.run()
-		objc.viewDidLoad()
+
+    @IBOutlet weak var image1: UIImageView!
+    @IBAction func button(sender: AnyObject) {
+        
+        var positionx = image1.frame.origin.x
+        var positiony = image1.frame.origin.y
+        
+        image1.frame = CGRect(x: positionx, y: positiony, width: image1.frame.size.width, height: image1.frame.size.height)
+        
+        positiony += -20
+
+                image1.frame = CGRect(x: positionx, y: positiony, width: image1.frame.size.width, height: image1.frame.size.height)
     }
-	
+    
+	override func viewDidLoad() {
+
+		var tab = self.tabBarController?.tabBar
+		tab?.barStyle = UIBarStyle.Black
+		var nav = self.navigationController?.navigationBar
+		nav?.barStyle = UIBarStyle.BlackTranslucent
+        
+		let screenSize: CGRect = UIScreen.mainScreen().bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        println("\(screenHeight) is the height and \(screenWidth) is the width. \(screenSize) is the screensize")
+        
+    }
+    
 	class View: UIView {
 	
 	
